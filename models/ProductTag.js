296d0012ada53a10d650b82f2324fbe771f7fbc5
Ragil14 <rahil.gilani@gmail.com>
2022-03-07 12:11:51 -0600

    add references to product & tag_id

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -10,13 +10,22 @@ ProductTag.init(
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
+      allowNull: false,
       autoIncrement: true
     },
     product_id: {
       type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
     },
     tag_id: {
       type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
     }
   },
   {
